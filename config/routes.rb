c70a356e706c515a69828677bf334855c16acf45
Paul Anthony McGowan <xhostcom@gmail.com>
2021-03-27 21:55:34 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :records
   resources :artists
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  reccordstore.com/api/v1/records
+ 
 end
