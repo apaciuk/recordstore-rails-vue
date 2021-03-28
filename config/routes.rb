19dfc071939b2b718cbf538edbc8daae512ff3a4
Paul Anthony McGowan <xhostcom@gmail.com>
2021-03-28 08:32:37 +0100

    Namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :records
-  resources :artists
-
-  reccordstore.com/api/v1/records
- 
+  
+  namespace :api do  
+    namespace :v1 do
+      resources :records
+      resources :artists
+    end
+  end
+  root to: "home#index"
 end
