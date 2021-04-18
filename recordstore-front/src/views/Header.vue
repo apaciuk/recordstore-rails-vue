3afec1770aefb8e8b1240ef2d67711d9eadb93ec
Paul Anthony McGowan <xhostcom@gmail.com>
2021-04-18 20:33:50 +0100

    Links

--- recordstore-front/src/views/Header.vue
+++ recordstore-front/src/views/Header.vue
@@ -7,18 +7,25 @@
     <span uk-search-icon></span>
     <input class="uk-search-input" type="search" placeholder="Search...">
 </form>
-            
-             
-     </li>
-        </ul>
+</li>
+ </ul>
    
     </div>
 
     <div class="uk-navbar-right">
       <ul class="uk-navbar-nav">
+           <li>
+    <router-link to="/" class="uk-navbar-item"> Sign Out</router-link>
+    </li>
+           <li>
+    <router-link to="/" class="uk-navbar-item"> Artists</router-link>
+    </li>
+       <li>
+    <router-link to="/" class="uk-navbar-item"> Records</router-link>
+    </li>
    
     <li>
-    <router-link to="/" class="uk-navbar-item uk-logo"> Sign In</router-link>
+    <router-link to="/" class="uk-navbar-item"> Sign In</router-link>
     </li>
          <li>
          <router-link to="/signup" class="uk-navbar-item">Register</router-link>
@@ -37,5 +44,7 @@ background: #494f5d;
 form.uk-search {
 margin-left: 30px;
 }
+a.uk-navbar-item {
 
+}
 </style>
